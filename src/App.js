306b565ab0a67744fa2026a862e3956f770f7f4a
Prahitha <prahitha.movva03@gmail.com>
2020-10-31 01:37:49 +0530

    added cute animations xD

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import { ThemeProvider } from '@material-ui/core/styles'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import theme from './theme'
-import Nav from './Components/nav'
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from './theme';
+import Nav from './Components/nav';
+import Spider from './Components/Spider/spider';
+import Pumpkin from './Components/Pumpkin/pumpkin';
+import Ghost from './Components/Ghost/ghost';
 
 function App() {
   return (
+    <div>
+      <Spider />
+      <Ghost />
+      <Pumpkin />
     <ThemeProvider theme={theme}>
     <CssBaseline />
       <Nav 
@@ -20,6 +27,7 @@ Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
           .join('\n')}
       />
   </ThemeProvider>
+  </div>
   );
 }
 
