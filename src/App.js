a170b04940d9af24f61dfea512da7d9f63f90d80
Meghana <meghana@iitbhilai.ac.in>
2020-10-30 22:53:41 +0530

    added theming-1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { ThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import theme from './theme'
+import Nav from './Components/nav'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+    <CssBaseline />
+      <Nav 
+        content={[...new Array(50)]
+          .map(
+            () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+          )
+          .join('\n')}
+      />
+  </ThemeProvider>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,33 @@
+import { createMuiTheme }  from '@material-ui/core/styles'
+const theme = createMuiTheme({
+  palette: {
+    primary: { 
+        main: '#200236' ,
+    },
+    secondary: {
+        main: '#AF4B00',
+    }
+  },
+  typography: {
+        body1:{
+            fontFamily: 'Creepster',
+        },
+        body2:{
+            fontFamily: 'Griffy', 
+        },
+  },
+//   overrides: {
+//     MuiCssBaseline: {
+//       '@global': {
+//         '@font-face': [raleway],
+//       },
+//     },
+//   },
+  body1: {
+    fontWeight: 500,
+  },
+  button: {
+    fontStyle: 'italic',
+  },
+})
+export default theme
