d5305d2fb7b3b628351ae92a2da763849706c4ab
Meghana <meghana@iitbhilai.ac.in>
2020-10-31 00:27:00 +0530

    added theming and home page(need to fix issues)

--- src/App.js
+++ src/App.js
@@ -4,21 +4,17 @@ import { ThemeProvider } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import theme from './theme'
 import Nav from './Components/nav'
+import Home from './Components/home';
+
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
     <CssBaseline />
-      <Nav 
-        content={[...new Array(50)]
-          .map(
-            () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-          )
-          .join('\n')}
-      />
+    <Nav >
+         <Home/>
+      </Nav>
+       
   </ThemeProvider>
   );
 }
