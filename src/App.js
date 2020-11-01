25da472d602b26d1170d8fb22b0e41bef24971e4
Meghana <meghana@iitbhilai.ac.in>
2020-11-01 13:17:25 +0530

    added /games and web cam app

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import { Route, Switch } from "react-router-dom";
 import Layout from "./layout";
 import Signup from "./Components/Signup/signup";
 import Login from "./Components/Login/login";
+import Games from "./Components/games";
+
 const App = () => {
   return (
     <Switch>
       <Route path="/signup" component={Signup} />
       <Route path="/login" component={Login} />
+      <Route path="/games" component={Games} />
       <Route path="/" component={Layout} />
     </Switch>
   );
---
+++ src/Components/games.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Card from './card.js';
+import { makeStyles } from '@material-ui/core/styles';
+import bg1 from '../Images/bg1.jpg';
+
+const useStyles = makeStyles((theme) => ({
+    root: { },
+}));
+export default function Games(){
+    return (
+        <div></div>
+    );
+}
